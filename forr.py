79a1d8968e6c3e3dab2e759add8b8cccef4a7131
Þröstur Haraldsson <throsturha309@nemi.tskoli.is>
2017-01-25 14:23:36 +0000

    gerði dæmi 3.

--- forr.py
+++ forr.py
@@ -1,6 +1,6 @@
 #Þröstur Haraldsson
 #25.1.2017
-
+"""
 #Dæmi1
 tala1=int(input("Sláðu inn tölu"))
 tala2=int(input("Sláðu inn aðra tölu"))
@@ -17,3 +17,21 @@ fornafn=input("Fornafn?")
 eftnafn=input("Eftirnafn?")
 print("Halló",fornafn,eftnafn)
 print("")
+"""
+#Dæmi3
+text=input("Texti:")
+telstor=0
+tellitill=0
+storlitill=0
+telAlla = 0
+for c in text:
+    if c.islower():
+        tellitill=tellitill+1
+    if c.isupper():
+        telstor=telstor+1
+        if (text[telAlla+1].islower()):
+            storlitill=storlitill+1
+    telAlla += 1
+print("Það eru",telstor,"stórir stafir")
+print("það eru",tellitill,"litlir stafir")
+print("Það eru",storlitill,"litlir stafir beint á eftir stórum")
