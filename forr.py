55504a0af90c6dfc01d2de1be9990e20bea77b19
Þröstur Haraldsson <throsturha309@nemi.tskoli.is>
2017-01-25 13:36:09 +0000

    Merkti og gerði dæmi 1.

---
+++ forr.py
@@ -0,0 +1,13 @@
+#Þröstur Haraldsson
+#25.1.2017
+
+#Dæmi1
+tala1=int(input("Sláðu inn tölu"))
+tala2=int(input("Sláðu inn aðra tölu"))
+samlagning=tala1 + tala2
+margfoldun=tala1 * tala2
+print("Ef þú leggur tölurnar saman þá færðu")
+print(samlagning)
+print("Ef þú margfaldar tölurnar saman þá færðu")
+print(margfoldun)
+print("")
